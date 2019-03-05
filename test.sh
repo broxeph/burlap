db03262b17ac55fc4478e04ba1e09297e13c32ec
Chris Spencer <chrisspen@gmail.com>
2019-03-05 18:16:57 -0500

    Updated local test script

--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Runs the entire test suite locally.
+#
 # To run a specific command:
 #
 #   tox -c tox-full.ini -- -s burlap/tests/test_common.py::CommonTests::test_iter_sites
 #
 set -e
-time ./pep8.sh
+[ -d .env ] && rm -Rf .env
+virtualenv .env
+. .env/bin/activate
+pip install -r requirements-test.txt
+./pep8.sh
 rm -Rf ./burlap/*.pyc
 time tox -c tox-full.ini
