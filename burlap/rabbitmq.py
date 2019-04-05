87fa9e85f0654471e6729c74d9a149f24cd4f7da
Chris Spencer <chrisspen@gmail.com>
2019-04-05 15:34:44 -0400

    Added inadyn satchel.

--- burlap/rabbitmq.py
+++ burlap/rabbitmq.py
@@ -310,6 +310,7 @@ class RabbitMQSatchel(ServiceSatchel):
         data = super(RabbitMQSatchel, self).record_manifest()
         params = sorted(list(self.get_user_vhosts())) # [(user, password, vhost)]
         data['rabbitmq_all_site_vhosts'] = params
+        data['sites'] = list(self.genv.sites or [])
         return data
 
     @task(precursors=['packager', 'user'])
