6f7dc6422641f7019c82a6205a06cdd11df61aa0
Alex Ball <aball@edgeondemand.com>
2019-05-15 15:55:22 -0400

    Add Ubuntu 18.04 compatibility to postgresql satchels

--- burlap/pip.py
+++ burlap/pip.py
@@ -126,16 +126,14 @@ class PIPSatchel(Satchel):
         Returns true if the virtual environment has been created.
         """
         r = self.local_renderer
-        ret = True
         with self.settings(warn_only=True):
             ret = r.run_or_local('ls {virtualenv_dir}') or ''
             ret = 'cannot access' not in ret.strip().lower()
 
-        if self.verbose:
-            if ret:
-                print('Yes')
-            else:
-                print('No')
+        if ret:
+            self.vprint('Yes')
+        else:
+            self.vprint('No')
 
         return ret
 
